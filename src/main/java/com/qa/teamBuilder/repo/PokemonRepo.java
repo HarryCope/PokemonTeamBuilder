cc826f1c44b4c73c8a2db696794bf923440ba65a
Harry <copeharryv16@gmail.com>
2022-08-09 19:02:28 +0100

    finished the api

--- src/main/java/com/qa/teamBuilder/repo/PokemonRepo.java
+++ src/main/java/com/qa/teamBuilder/repo/PokemonRepo.java
@@ -1,17 +1,20 @@
 package com.qa.teamBuilder.repo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import com.qa.teamBuilder.models.Pokemon;
 
-@Repository
-public class PokemonRepo {
-
-	public interface CatRepo extends JpaRepository<Pokemon, Long> {
+	@Repository
+	public interface PokemonRepo extends JpaRepository<Pokemon, Long> {
 		
+		@Query(value = "SELECT * FROM pokemon", nativeQuery = true)
+		public List<Pokemon> readAllCats();
 		
+		@Query(value = "SELECT * FROM pokemon WHERE name = ?1", nativeQuery = true)
+		public List<Pokemon> sameNameSelect(String name);
 	}
 	
-	
-}
+
